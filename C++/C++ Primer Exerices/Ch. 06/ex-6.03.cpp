fd77a137c7a0e5cf0033018fe0fff2a755b7a866
Kat <katisntgood@gmail.com>
2021-03-06 21:07:35 -0500

    update

---
+++ C++/C++ Primer Exerices/Ch. 06/ex-6.03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int fact(int val) {
+  int ret = 1; // return variable, set to one so it always returns
+  while (val > 1) {
+    ret *= val--;
+  }
+  return ret;
+}
+
+int main() {
+  int i;
+  std::cout << "Enter a number: ";
+  while (std::cin >> i) {
+    int val = fact(i);
+    std::cout << "The factorial of " << i << " is: " << val << std::endl;
+    return 0;
+  }
+}
